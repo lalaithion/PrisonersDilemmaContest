0ee315557e03a60a89614a7e96e2c94fa12d2d23
lalaithion <izaak.weiss@gmail.com>
2016-01-10 17:28:54 -0700

    Player Base Class

---
+++ PlayerBaseClass.py
@@ -0,0 +1,14 @@
+class Player:
+	def play():
+		'''
+		returns a boolean value indicating 
+		cooperation (True) or defection (False).
+		'''
+		return True #Cooperate Bot
+	def reset():
+		'''
+		this resets any relevant data upon being 
+		assigned a new opponent, any returned value 
+		will be ignored
+		'''
+		return #returns None
